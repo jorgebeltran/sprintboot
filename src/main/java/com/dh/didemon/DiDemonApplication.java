79aee8f2a72306bd5527a7475828307489f858a5
jorge <yo_coqui@hotmail.com>
2018-04-26 20:17:38 -0400

    mi primer hello word sprint

---
+++ src/main/java/com/dh/didemon/DiDemonApplication.java
@@ -0,0 +1,21 @@
+package com.dh.didemon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping
+@SpringBootApplication
+@RestController
+public class DiDemonApplication {
+	@RequestMapping("/")
+	String home() {
+		return "Hello World!";
+	}
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiDemonApplication.class, args);
+	}
+}
